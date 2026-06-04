306966da46d1d01a39d7d5453da5dd7be41fff1e
Dudley Wong <dnwong@gmail.com>
2026-06-04 11:00:36 -0400

    Shift window to 4:35-4:45pm to exclude 16:30 bar

--- fetcher.js
+++ fetcher.js
@@ -188,10 +188,10 @@ async function fetchAndFill(symbol) {
   const prevMidnight  = prevRTHBars.length ? etMidnightUTC(prevRTHBars[0].t) : todayMidnight - 86400000;
 
   const onBars = intraBars.filter(b => b.t > prevMidnight + RTH_END   && b.t <= todayMidnight + RTH_START);
-  // Late day: PREVIOUS trading day 4:30pm–4:45pm ET
+  // Late day: PREVIOUS trading day 4:35pm–4:45pm ET
   // Saul uses this post-market window for his floor pivot (H+L+C)/3
   const allLdBars = intraBars
-    .filter(b => b.t >= prevMidnight + 16.5*3600000 && b.t <= prevMidnight + 16.75*3600000)
+    .filter(b => b.t >= prevMidnight + 16.583*3600000 && b.t <= prevMidnight + 16.75*3600000)
     .sort((a, b) => a.t - b.t);
 
   const ldBars = allLdBars;
